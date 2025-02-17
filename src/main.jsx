8bfe1019e4d6770da63a579cb1baa52ee1ffca92
MuhammedMGX <muhammedgharib3000@gmail.com>
2025-02-17 13:07:05 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'flowbite/dist/flowbite.min.js';
+
 
 
 import './../node_modules/@fortawesome/fontawesome-free/css/all.css'
